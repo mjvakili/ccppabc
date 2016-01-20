94b9235ddb292a377a35f330ae637d75cfdf8c1f
changhoonhahn <chh327@nyu.edu>
2016-01-20 21:20:48 +0000

    updates to fake observations

--- halo/code/data.py
+++ halo/code/data.py
@@ -1,65 +1,162 @@
 '''
 
-Modules for data and observables in ABC-PMC
+Modules for data and observables in ABC-PMC. 
 
 '''
 import numpy as np
+from group_richness import gmf_bins
+from group_richness import richness 
+from group_richness import gmf as GMF
+from halotools.empirical_models import PrebuiltHodModelFactory
 
-def data_gmf(Mr=20): 
+def data_gmf(Mr=20, Nmock=500): 
+    ''' Observed GMF from 'data'
     '''
-    Observed GMF from 'data'
+    gmf_dat_file = ''.join(['../dat/gmf.Mr', str(Mr), '.dat'])
+    gmf_sig_file = ''.join(['../dat/gmf_sigma.Mr', str(Mr), '.Nmock', str(Nmock), '.dat'])
+    return [np.loadtxt(gmf_dat_file), np.loadtxt(gmf_sig_file)]
+
+def data_gmf_bins(): 
+    ''' Just for consistency  
     '''
+    return gmf_bins()
 
-    gmf_dat_file = ''.join([
-        '../dat/', 
-        'gmf_Mr', str(Mr), '.dat'
-        ])
-    
-    gmf_sig_file = ''.join([
-        '../dat/', 
-        'gmf_noise_Mr20_2.dat'
-        ])
-    sigma = np.loadtxt(gmf_sig_file)
-    
-    return [np.loadtxt(gmf_dat_file), sigma]
-
-def data_gmf_bins(Mr=20):
-    '''
-    Observed GMF bins
-    '''
-    bins = np.array([3.000000000000000000e+00, 4.000000000000000000e+00, 5.000000000000000000e+00, 6.000000000000000000e+00, 7.000000000000000000e+00, 8.000000000000000000e+00, 9.000000000000000000e+00, 1.000000000000000000e+01, 1.100000000000000000e+01, 1.200000000000000000e+01, 1.300000000000000000e+01, 1.400000000000000000e+01, 1.500000000000000000e+01, 1.600000000000000000e+01, 1.700000000000000000e+01, 1.800000000000000000e+01, 1.900000000000000000e+01, 2.000000000000000000e+01, 2.100000000000000000e+01, 2.200000000000000000e+01, 2.300000000000000000e+01, 2.500000000000000000e+01, 2.900000000000000000e+01, 3.100000000000000000e+01, 3.500000000000000000e+01, 4.300000000000000000e+01, 6.100000000000000000e+01]) # same hardcoded bins as data 
-
-    return bins
-
-def data_nbar(Mr=20): 
+def data_nbar(Mr=20, Nmock=500): 
     '''
     Observed nbar from 'data'
     '''
-    nbar_dat_file = ''.join([
-        '../dat/',
-        'nbar_Mr', str(Mr),'.dat'
-        ])
-    mock_nbar = np.loadtxt(nbar_dat_file)
+    nbar = np.loadtxt(''.join(['../dat/nbar.Mr', str(Mr), '.dat']))
+    nbar_cov = np.loadtxt(''.join(['../dat/nbar_cov.Mr', str(Mr), '.Nock', str(Nmock), '.dat']))
+    return [nbar, nbar_cov]
 
-    return [np.mean(mock_nbar), np.var(mock_nbar)]
-
-def data_xi(Mr=20): 
+def data_xi(Mr=20, Nmock=500): 
     '''
     Observed xi (2PCF) from 'data'
     '''
-    xi_dat_file = ''.join([
-        '../dat/', 
-        'xir_Mr', str(Mr), '.dat'
-        ])
-
+    xi_dat_file = ''.join(['../dat/xir.Mr', str(Mr), '.dat'])
     xi = np.loadtxt(xi_dat_file, unpack=True)
 
     # load covariance of xi 
-    cov_dat_file = ''.join([
-        '../dat/', 
-        'clustering_covariance_Mr', str(Mr), '.dat'
-        ])
+    cov_dat_file = ''.join(['../dat/xir_covariance.Mr', str(Mr), '.Nmock', str(Nmock), '.dat'])
     cov = np.loadtxt(cov_dat_file)
     cii = np.diag(covariance)   # diagonal elements
 
     return [xi, cii]
+    
+
+# Build observables ---------------
+
+def build_xi(Mr=20): 
+    '''
+    Build "data" xi values 
+    '''
+    model = PrebuiltHodModelFactory('zheng07', threshold = -1.0*np.float(Mr))
+    model.populate_mock() 
+    data_xir  = model.mock.compute_galaxy_clustering()[1]
+    
+    output_file = ''.join(['../dat/xir.Mr', str(Mr), '.dat'])
+
+    np.savetxt(output_file, data_xir)
+    return None 
+
+def build_xi_cov(Mr=20, Nmock=500): 
+    '''
+    Build covariance matrix for xi, using Nmock simulations 
+    ''' 
+    for i in xrange(Nmock): 
+        model = PrebuiltHodModelFactory('zheng07', threshold = -1.0*np.float(Mr))
+        model.populate_mock() 
+
+        xir.append(model.mock.compute_galaxy_clustering()[1])
+
+    covar = np.cov(np.array(xir).T)
+    output_file = ''.join(['../dat/xir_covariance.Mr', str(Mr), '.Nmock', str(Nmock), '.dat'])
+
+    np.savetxt(output_file, covar)
+    
+    return None
+
+def build_nbar(Mr=20): 
+    ''' Build observed nbar value
+    '''
+    model = PrebuiltHodModelFactory('zheng07', threshold = -1.0*np.float(Mr))
+    model.populate_mock() 
+    nbar = model.mock.number_density
+    
+    # save nbar values 
+    output_file = ''.join(['../dat/nbar.Mr', str(Mr), '.dat'])
+    np.savetxt(output_file, nbar) 
+    return None 
+
+def build_nbar_cov(Mr=20, Nmock=500): 
+    ''' Build observed nbar value
+    '''
+    model = PrebuiltHodModelFactory('zheng07', threshold = -1.0*np.float(Mr))
+    nbars = [] 
+    for i in xrange(Nmock): 
+        model.populate_mock() 
+        nbars.append(model.mock.number_density)
+    
+    nbar_cov = np.var(nbars, axis=0) 
+
+    # save nbar values 
+    output_file = ''.join(['../dat/nbar_cov.Mr', str(Mr), '.Nmock', str(Nmock), '.dat'])
+    np.savetxt(output_file, nbar_cov) 
+    return None
+
+def build_gmf(Mr=20): 
+    ''' Build 'observed' GMF. This is from a single realization of the mock 
+    '''
+    model = PrebuiltHodModelFactory('zheng07', threshold = -1.0*np.float(Mr))
+    
+    model.populate_mock() 
+    rich = richness(model.mock.compute_fof_group_ids())
+    gmf = GMF(rich)  # GMF
+
+    # save to file 
+    output_file = ''.join(['../dat/gmf.Mr', str(Mr), '.dat'])
+    np.savetxt(output_file, np.array(gmf)) 
+    return None
+    
+def build_gmf_sigma(Mr=20, Nmock=500): 
+    ''' Build 'observed' uncertainty in GMF from Nmock simulated realizations. 
+    Uncertainty is the quadruture of poisson errors and stddev. 
+    '''
+    model = PrebuiltHodModelFactory('zheng07', threshold = -1.0*np.float(Mr))
+    
+    gmfs = [] 
+    gmf_counts = [] 
+    for i in xrange(Nmock): 
+        model.populate_mock() 
+        rich = richness(model.mock.compute_fof_group_ids())
+        gmfs.append(GMF(rich))  # GMF
+        gmf_counts.append(gmf(rich, counts=True))   # Group counts 
+
+    gmf_counts_mean = np.mean(gmf_counts, axis=0)
+    poisson_gmf = np.sqrt(gmf_counts_mean) / 250.**3    # poisson errors
+    sigma_gmf = np.std(gmfs, axis=0)                    # sample variance 
+
+    sigma_tot = (sigma_gmf**2 + poisson_gmf**2)**0.5    # total sigma
+
+    # save to file  
+    output_file = ''.join(['../dat/gmf_sigma.Mr', str(Mr), '.Nmock', str(Nmock), '.dat'])
+    np.savetxt(output_file, sigma_tot) 
+    return None
+
+def build_observations(Mr=20, Nmock=500): 
+    ''' Build all the fake observations
+    '''
+    # xi 
+    build_xi(Mr=Mr)
+    build_xi_cov(Mr=Mr, Nmock=Nmock)
+    # nbar
+    build_nbar(Mr=Mr)
+    build_nbar_cov(Mr=Mr, Nmock=Nmock)
+    # gmf
+    build_gmf(Mr=Mr)
+    build_gmf_sigma(Mr=Mr, Nmock=Nmock)
+    return None
+
+
+if __name__=='__main__': 
+    build_observations(Mr=20, Nmock=5)
