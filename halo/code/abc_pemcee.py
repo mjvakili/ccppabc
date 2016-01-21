c37853925f0523de11fe9b30942d4a8704ec3c6d
ChangHoon Hahn <chh327@nyu.edu>
2016-01-20 22:08:58 -0500

    ABC-PMC updated

--- halo/code/abc_pemcee.py
+++ halo/code/abc_pemcee.py
@@ -13,20 +13,30 @@ from group_richness import richness
 # --- Plotting ---
 from plotting import plot_thetas
 
-def abcpmc_nbar_gmf(T, eps_val, Mr=20, N_part=1000, threads=1, observables=['nbar', 'gmf']): 
+def abcpmc(T, eps_val, N_part=1000, threads=1, observables=['nbar', 'gmf'], data_dict={'Mr':20, 'Nmock': 500}):
     '''
+    ABC-PMC implementation. 
+
+    Parameters
+    ----------
+    - T : Number of iterations 
+    - eps_val : ???? (@mjv: what does this do?)
+    - N_part : Number of particles
+    - Threads : Number of MPI threads (not sure if this keyword actually works or not 
+    - observables : list of observables. Options are 'nbar', 'gmf', 'xi'
+    - data_dict : dictionary that specifies the observation keywords 
     '''
     # data observables
     data = []       # list of observables 
     for obv in observables: 
         if obv == 'nbar': 
-            data_nbar, data_nbar_var = Data.data_nbar(Mr=Mr)
+            data_nbar, data_nbar_var = Data.data_nbar(**data_dict)
             data.append(data_nbar)
         if obv == 'gmf': 
-            data_gmf, data_gmf_sigma = Data.data_gmf(Mr=Mr)
+            data_gmf, data_gmf_sigma = Data.data_gmf(**data_dict)
             data.append(data_gmf)
         if obv == 'xi': 
-            data_xi, data_cov_ii = Data.data_xi(Mr=Mr)
+            data_xi, data_cov_ii = Data.data_xi(**data_dict)
             data.append(data_xi)
     
     # True HOD parameters
@@ -46,8 +56,8 @@ def abcpmc_nbar_gmf(T, eps_val, Mr=20, N_part=1000, threads=1, observables=['nba
     # simulator
     our_model = HODsim()    # initialize model
     kwargs = {'prior_range': prior_range, 'observables': observables}
-    def simz(theta): 
-        return our_model.sum_stat(theta, **kwargs)
+    def simz(tt): 
+        return our_model.sum_stat(tt, **kwargs)
 
     def multivariate_rho(datum, model): 
         dists = [] 
@@ -60,6 +70,7 @@ def abcpmc_nbar_gmf(T, eps_val, Mr=20, N_part=1000, threads=1, observables=['nba
                 dists.append(dist_gr)
             if obv == 'xi': 
                 dist_xi = np.sum((datum[i_obv] - model[i_obv])**2. / data_cov_ii)
+                dists.append(dist_xi)
         return np.array(dists)
 
     mpi_pool = mpi_util.MpiPool()
@@ -72,17 +83,22 @@ def abcpmc_nbar_gmf(T, eps_val, Mr=20, N_part=1000, threads=1, observables=['nba
             pool=mpi_pool)  
     abcpmc_sampler.particle_proposal_cls = abcpmc.ParticleProposal
 
-    eps = abcpmc.ConstEps(T, [1.e13,1.e13])
+    #eps = abcpmc.ConstEps(T, [1.e13,1.e13])
+    eps = abcpmc.ConstEps(T, eps_val)       # (@mjv: what does this do?)
     pools = []
     for pool in abcpmc_sampler.sample(prior, eps):
         print("T:{0},ratio: {1:>.4f}".format(pool.t, pool.ratio))
         print eps(pool.t)
-
-        plot_thetas(pool.thetas , pool.ws , pool.t, Mr=Mr, truths=data_hod, plot_range=prior_range, observables=observables)
-        np.savetxt(''.join([util.dat_dir(), util.observable_id_flag(observables), 
-            '_Mr', str(Mr), '_theta_t', str(pool.t), '.dat']), theta)
-        np.savetxt(''.join([util.dat_dir(), util.observable_id_flag(observables), 
-            '_Mr', str(Mr), '_w_t', str(pool.t), '.dat']), w)
+        # plot theta
+        plot_thetas(pool.thetas, pool.ws , pool.t, 
+                Mr=Mr, truths=data_hod, plot_range=prior_range, observables=observables)
+        # write theta and w to file 
+        theta_file = ''.join([util.dat_dir(), util.observable_id_flag(observables), 
+            '_Mr', str(Mr), '_theta_t', str(pool.t), '.dat'])
+        w_file = ''.join([util.dat_dir(), util.observable_id_flag(observables), 
+            '_Mr', str(Mr), '_w_t', str(pool.t), '.dat'])
+        np.savetxt(theta_file, pooltheta)
+        np.savetxt(w_file, pool.ws)
 
         if pool.t < 3: 
             eps.eps = np.percentile(np.atleast_2d(pool.dists), 50 , axis = 0)
@@ -101,4 +117,4 @@ def abcpmc_nbar_gmf(T, eps_val, Mr=20, N_part=1000, threads=1, observables=['nba
     return pools
 
 if __name__=="__main__": 
-    abcpmc_nbar_gmf(10, 60, N_part=100)
+    abcpmc(10, 60, N_part=100)
