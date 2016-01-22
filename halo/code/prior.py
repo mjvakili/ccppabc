1d8a47fb9fa2b73ff4c065cff97ffb36e3e68286
changhoonhahn <changh20@gmail.com>
2016-01-22 18:14:49 +0000

    prior.py handles priors

---
+++ halo/code/prior.py
@@ -0,0 +1,30 @@
+'''
+
+Module to deal with priors 
+
+'''
+
+def prior_range(prior_name): 
+    ''' Given prior dictionary name, return the prior range. 
+    '''
+    
+    dict_table = prior_dict_table() 
+
+    prior_dict = dict_table[prior_name]
+
+    prior_min = prior_dict['prior_min']
+    prior_max = prior_dict['prior_max']
+    return [prior_min, prior_max]
+
+
+def prior_dict_table(): 
+    ''' dictionary table of priors 
+    '''
+    dict_table = { 
+            'first_try': {
+                'prior_min': [10., np.log(0.1), 11.02, 0.8, 13.]
+                'prior_max': [13., np.log(0.7), 13.02, 1.3, 14.]
+                }
+            }
+            
+    return dict_table 
