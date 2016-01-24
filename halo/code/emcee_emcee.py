04abaac9db97c95ceb40ed9c1210e781ada4ca25
changhoonhahn <changh20@gmail.com>
2016-01-24 03:27:55 +0000

    mcmc fixed?

--- halo/code/emcee_emcee.py
+++ halo/code/emcee_emcee.py
@@ -21,8 +21,13 @@ from group_richness import richness
 from prior import PriorRange
     
 def lnPost(theta, **kwargs):
-    '''log-posterior
+    '''log Posterior 
     '''
+    fake_obs = kwargs['data']
+    fake_obs_cov = kwargs['data_cov']
+    kwargs.pop('data', None)
+    kwargs.pop('data_cov', None)
+    observables = kwargs['observables']
     prior_range = kwargs['prior_range']
     prior_min = prior_range[:,0]
     prior_max = prior_range[:,1]
@@ -53,19 +58,20 @@ def lnPost(theta, **kwargs):
         res_xi = fake_obs[ind] - model_obvs[ind]
     
     chi_tot = 0.
+    ind = 0 
     if 'nbar' in observables: 
-        chi_tot += -0.5*(res_nbar)**2. / data_nbar_var 
+        chi_tot += -0.5*(res_nbar)**2. / fake_obs_cov[ind] 
+        ind += 1
     if 'gmf' in observables: 
         raise NotImplementedError('GMF Likelihood has not yet been implemented')
     if 'xi' in observables: 
-        chi_tot += -0.5*np.sum(np.dot(np.dot(res_xi , data_xi_invcov) , res_xi))
+        chi_tot += -0.5*np.sum(np.dot(np.dot(res_xi , fake_obs_cov[ind]) , res_xi))
     lnLike = chi_tot
 
     return lnPrior + lnLike
 
-
-def mcmc(Nwalkers, Nchains_burn, Nchains_pro, observables=['nbar', 'xi'], 
-        data_dict={'Mr':20, 'Nmock':500}, prior_name = 'first_try', N_threads=1): 
+def mcmc_mpi(Nwalkers, Nchains_burn, Nchains_pro, observables=['nbar', 'xi'], 
+        data_dict={'Mr':20, 'Nmock':500}, prior_name = 'first_try'): 
     '''
     Standard MCMC implementaion
     
@@ -84,18 +90,22 @@ def mcmc(Nwalkers, Nchains_burn, Nchains_pro, observables=['nbar', 'xi'],
     '''
     # data observables
     fake_obs = []       # list of observables 
+    fake_obs_cov = [] 
     for obv in observables: 
         if obv == 'nbar': 
             data_nbar, data_nbar_var = Data.data_nbar(**data_dict)
             fake_obs.append(data_nbar)
+            fake_obs_cov.append(data_nbar_var)
         if obv == 'gmf': 
             data_gmf, data_gmf_sigma = Data.data_gmf(**data_dict)
             fake_obs.append(data_gmf)
+            fake_obs_cov.append(data_gmf)
         if obv == 'xi': 
             # import xir and full covariance matrix of xir
             data_xi, data_xi_cov = Data.data_xi_full_cov(**data_dict)   
             data_xi_invcov = Data.data_xi_inv_cov(**data_dict)
             fake_obs.append(data_xi)
+            fake_obs_cov.append(data_xi_invcov)
 
     # True HOD parameters
     data_hod_dict = Data.data_hod_param(Mr=data_dict['Mr'])
@@ -119,33 +129,110 @@ def mcmc(Nwalkers, Nchains_burn, Nchains_pro, observables=['nbar', 'xi'],
     pos0 = np.repeat(random_guess, Nwalkers).reshape(Ndim, Nwalkers).T + \
             1e-1 * np.random.randn(Ndim * Nwalkers).reshape(Nwalkers, Ndim)
 
-    ## Initializing MPIPool
-    #pool = MPIPool()
-    #if not pool.is_master():
-    #    pool.wait()
-    #    sys.exit(0)
+    # Initializing MPIPool
+    pool = MPIPool()
+    if not pool.is_master():
+        pool.wait()
+        sys.exit(0)
 
     # Initializing the emcee sampler
-    hod_kwargs = {'prior_range': prior_range, 'observables': observables, 'Mr': data_dict['Mr']}
-    #sampler = emcee.EnsembleSampler(Nwalkers, Ndim, lnPost, pool=pool, kwargs=[hod_kwargs])
-    sampler = emcee.EnsembleSampler(Nwalkers, Ndim, lnPost, kwargs=hod_kwargs, threads=N_threads)
-    sampler.run_mcmc(pos0, Nchains_burn + Nchains_pro) 
+    hod_kwargs = {
+            'prior_range': prior_range, 
+            'data': fake_obs, 
+            'data_cov': fake_obs_cov, 
+            'observables': observables, 
+            'Mr': data_dict['Mr']
+            }
+    sampler = emcee.EnsembleSampler(Nwalkers, Ndim, lnPost, pool=pool, kwargs=hod_kwargs)
+    #sampler = emcee.EnsembleSampler(Nwalkers, Ndim, lnPost, kwargs=hod_kwargs, threads=N_threads)
+    #sampler.run_mcmc(pos0, Nchains_burn + Nchains_pro) 
     
-    #"""Running the sampler and saving the chains incrementally"""
-    #f = open("hod_chain.dat", "w")
-    #f.close()
-    #for result in sampler.sample(pos0, iterations = Nchains_burn + Nchains_pro, storechain=False):
-    #    position = result[0]
-    #    print position.shape
-    #    f = open("hod_chain.dat", "a")
-    #    for k in range(position.shape[0]):
-    #        output_str = '\t'.join(position[k].astype('str')) + '\n'
-    #        f.write(output_str)
-    #    f.close()
+    # Running the sampler and saving the chains incrementally
+    f = open("hod_chain.dat", "w")
+    f.close()
+    for result in sampler.sample(pos0, iterations = Nchains_burn + Nchains_pro, storechain=False):
+        position = result[0]
+        print position.shape
+        f = open("hod_chain.dat", "a")
+        for k in range(position.shape[0]):
+            output_str = '\t'.join(position[k].astype('str')) + '\n'
+            f.write(output_str)
+        f.close()
 
 
     #sampler = emcee.EnsembleSampler(Nwalkers, Ndim, lnPost)
-    #pool.close()
+    pool.close()
     
+def mcmc_multi(Nwalkers, Nchains_burn, Nchains_pro, observables=['nbar', 'xi'], 
+        data_dict={'Mr':20, 'Nmock':500}, prior_name = 'first_try', threads=1): 
+    '''
+    Standard MCMC implementaion
+    
+
+    Parameters
+    -----------
+    - Nwalker : 
+        Number of walkers
+    - Nchains_burn : 
+        Number of burn-in chains
+    - Nchains_pro : 
+        Number of production chains   
+    - observables : 
+        list of observables. Options are 'nbar', 'gmf', 'xi'
+    - data_dict : dictionary that specifies the observation keywords
+    '''
+    # data observables
+    fake_obs = []       # list of observables 
+    fake_obs_cov = [] 
+    for obv in observables: 
+        if obv == 'nbar': 
+            data_nbar, data_nbar_var = Data.data_nbar(**data_dict)
+            fake_obs.append(data_nbar)
+            fake_obs_cov.append(data_nbar_var)
+        if obv == 'gmf': 
+            data_gmf, data_gmf_sigma = Data.data_gmf(**data_dict)
+            fake_obs.append(data_gmf)
+            fake_obs_cov.append(data_gmf)
+        if obv == 'xi': 
+            # import xir and full covariance matrix of xir
+            data_xi, data_xi_cov = Data.data_xi_full_cov(**data_dict)   
+            data_xi_invcov = Data.data_xi_inv_cov(**data_dict)
+            fake_obs.append(data_xi)
+            fake_obs_cov.append(data_xi_invcov)
+
+    # True HOD parameters
+    data_hod_dict = Data.data_hod_param(Mr=data_dict['Mr'])
+    data_hod = np.array([
+        data_hod_dict['logM0'],                 # log M0 
+        np.log(data_hod_dict['sigma_logM']),    # log(sigma)
+        data_hod_dict['logMmin'],               # log Mmin
+        data_hod_dict['alpha'],                 # alpha
+        data_hod_dict['logM1']                  # log M1
+        ])
+    Ndim = len(data_hod)
+    
+    # Priors
+    prior_min, prior_max = PriorRange(prior_name)
+    prior_range = np.zeros((len(prior_min),2))
+    prior_range[:,0] = prior_min
+    prior_range[:,1] = prior_max
+    
+    # Initializing Walkers (@mjv : f@#k for loops)
+    random_guess = np.array([11. , np.log(.4) , 11.5 , 1.0 , 13.5])
+    pos0 = np.repeat(random_guess, Nwalkers).reshape(Ndim, Nwalkers).T + \
+            1e-1 * np.random.randn(Ndim * Nwalkers).reshape(Nwalkers, Ndim)
+
+    # Initializing the emcee sampler
+    hod_kwargs = {
+            'prior_range': prior_range, 
+            'data': fake_obs, 
+            'data_cov': fake_obs_cov, 
+            'observables': observables, 
+            'Mr': data_dict['Mr']
+            }
+    sampler = emcee.EnsembleSampler(Nwalkers, Ndim, lnPost, kwargs=hod_kwargs, threads=threads)
+    sampler.run_mcmc(pos0, Nchains_burn + Nchains_pro) 
+
+
 if __name__=="__main__": 
-    mcmc(10, 1, 1, observables=['nbar', 'xi'], N_threads=10)
+    mcmc_mpi(10, 1, 1, observables=['nbar', 'xi'])
