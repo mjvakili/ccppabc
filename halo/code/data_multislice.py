7bf37ded91204502a58b767cebd8e45a6754e6e5
mjvakili <mjvakili@nyu.edu>
2016-03-08 23:54:25 +0000

    corrected fofgroup calculations in non-periodic sub-boxes

--- halo/code/data_multislice.py
+++ halo/code/data_multislice.py
@@ -12,6 +12,7 @@ from numpy.linalg import solve
 from halotools.sim_manager import CachedHaloCatalog
 from halotools.empirical_models import PrebuiltHodModelFactory
 from halotools.mock_observables import tpcf
+from halotools.mock_observables import FoFGroups
 from halotools.empirical_models.factories.mock_helpers import three_dim_pos_bundle
 from halotools.mock_observables.pair_counters import npairs
 
@@ -282,7 +283,18 @@ def build_nbar_xi_gmf(Mr=21):
 
     #compute group richness    
 
-    rich = richness(model.mock.compute_fof_group_ids())
+    galaxy_sample = model.mock.galaxy_table
+    x = galaxy_sample['x']
+    y = galaxy_sample['y']
+    z = galaxy_sample['z']
+    vz = galaxy_sample['vz']
+    pos = three_dim_pos_bundle(model.mock.galaxy_table, 'x', 'y', 'z'
+                               , velocity = vz , velocity_distortion_dimension="z")
+    b_para, b_perp = 0.7, 0.15
+    groups = FoFGroups(pos, b_perp, b_para, period = None, 
+                      Lbox = 200 , num_threads='max')
+    gids = groups.group_ids
+    rich = richness(gids)
     gmf = GMF(rich)  # GMF
 
     fullvec = np.append(fullvec, gmf)
@@ -359,7 +371,18 @@ def build_nbar_xi_gmf_cov(Mr=21):
 	    NR_precomputed = NR)
         xir.append(xi)
         # calculate gmf
-        rich = richness(model.mock.compute_fof_group_ids())
+        galaxy_sample = model.mock.galaxy_table
+        x = galaxy_sample['x']
+        y = galaxy_sample['y']
+        z = galaxy_sample['z']
+        vz = galaxy_sample['vz']
+        pos = three_dim_pos_bundle(model.mock.galaxy_table, 'x', 'y', 'z'
+                               , velocity = vz , velocity_distortion_dimension="z")
+        b_para, b_perp = 0.7, 0.15
+        groups = FoFGroups(pos, b_perp, b_para, period = None, 
+                      Lbox = 200 , num_threads='max')
+        gids = groups.group_ids
+        rich = richness(gids)
         gmfs.append(GMF(rich))  # GMF
         gmf_counts.append(GMF(rich, counts=True))   # Group counts
 
