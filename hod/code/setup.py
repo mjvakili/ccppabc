99e794cad2e646cd6bffff156880bda1ea6efd2d
Chang Hoon Hahn <chh327@login-0-3.local>
2016-04-18 19:36:47 -0400

    setup.py fixed

--- hod/code/setup.py
+++ hod/code/setup.py
@@ -26,15 +26,16 @@ for dir in ['observations', 'pmc_abc', 'mcmc', 'crash']:
 
 build_obvs = raw_input("Do you want to download the observations [0] or build them from scratch [1] (takes ~2 hours)?") 
 
-if build_obvs == 0:
+if int(build_obvs) == 0:
     overwrite = raw_input("Overwrite the observations? [y/n]")
     if overwrite == 'y': 
         print 'Downloading observations ... '
-        subprocess.call(['wget', 'http://physics.nyu.edu/~chh327/data/ccppabc/*.dat', '-P', '../dat/observations/'])
+	for file in ['ABC.nbar_xi_gmf_cov.no_poisson.Mr21.bnorm0.25.dat', 'MCMC.nbar_xi_gmf_cov.no_poisson.Mr21.bnorm0.25.dat', 'RR.dat', 'abc_nbar_var.Mr21.dat', 'data_vector.Mr21.bnorm0.25.dat', 'nbar_var.Mr21.dat', 'randoms.dat', 'xir_rbin.Mr21.dat']: 
+		subprocess.call(['wget', 'http://physics.nyu.edu/~chh327/data/ccppabc/'+file, '-P', '../dat/observations/'])
     else: 
         print "Then do it yourself manually @ http://physics.nyu.edu/~chh327/data/ccppabc/"
 
-elif build_obvs == 1: 
+elif int(build_obvs) == 1: 
     print 'Downloading randoms ... '
     if not os.path.exists('../dat/observations/randoms.dat'):
         subprocess.call(['wget', 'http://physics.nyu.edu/~chh327/data/ccppabc/randoms.dat', '-P', '../dat/observations/'])
