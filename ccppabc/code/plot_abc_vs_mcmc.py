5ee8cf6f4a6cde1d25833a48eb5746ea715bf29a
mjvakili <mjvakili@nyu.edu>
2016-05-06 19:06:33 +0000

    fixed the comparison plots

--- ccppabc/code/plot_abc_vs_mcmc.py
+++ ccppabc/code/plot_abc_vs_mcmc.py
@@ -62,9 +62,9 @@ def overlay_pdfs_contours(abc_filename , mcmc_filename , nwalkers , nburns , Mr)
    
     ax = axes[0]
     ax_list = corner.hist2d(mcmc_sample[:,3],mcmc_sample[:,2],bins =20,levels=[0.68,0.95], ax = ax, plot_datapoints = False , 
-                            fill_contours = True, alpha = 10. , color = 'r', smooth = 1. , linewidth=4)
+                            fill_contours = True, alpha = 10. , color = 'r', smooth = 1. , linewidth=4, range = [plot_range[3,:],plot_range[2,:]])
     ax_list = corner.hist2d(abc_sample[:,3],abc_sample[:,2],bins =20,levels=[0.68,0.95], ax = ax, plot_datapoints = False , 
-                            fill_contours = True,alpha = 0.1, color = 'b', smooth = 1. , linewidth=4)
+                            fill_contours = True,alpha = 0.1, color = 'b', smooth = 1. , linewidth=4, range = [plot_range[3,:],plot_range[2,:]])
     ax.plot(truths[3] , truths[2] , marker="*", markersize=25 , color = "yellow")
     ax.set_ylabel(r'$\log M_{\rm min}$', fontsize = 50)
     ax.set_xlabel(r'$\alpha$', fontsize = 50)
@@ -74,9 +74,9 @@ def overlay_pdfs_contours(abc_filename , mcmc_filename , nwalkers , nburns , Mr)
 
     ax = axes[1]
     ax_list = corner.hist2d(mcmc_sample[:,2],mcmc_sample[:,4],bins =20,levels=[0.68,0.95], ax = ax, plot_datapoints = False , 
-                            fill_contours = True, color = 'r', smooth = 1. , linewidth=4)
+                            fill_contours = True, color = 'r', smooth = 1. , linewidth=4, range = [plot_range[2,:],plot_range[4,:]])
     ax_list = corner.hist2d(abc_sample[:,2],abc_sample[:,4],bins =20,levels=[0.68,0.95], ax = ax, plot_datapoints = False , 
-                           fill_contours = True, color = 'b', smooth = 1. , linewidth=4)
+                           fill_contours = True, color = 'b', smooth = 1. , linewidth=4, range = [plot_range[2,:],plot_range[4,:]])
     ax.plot(truths[2] , truths[4] , marker="*", markersize=25 , color = "yellow")
     ax.set_xlabel(r'$\log M_{\rm min}$', fontsize = 50)
     ax.set_ylabel(r'$\log M_{1}$', fontsize = 50)
@@ -87,9 +87,9 @@ def overlay_pdfs_contours(abc_filename , mcmc_filename , nwalkers , nburns , Mr)
 
     ax = axes[2]
     ax_list = corner.hist2d(mcmc_sample[:,3],mcmc_sample[:,4],bins =20,levels=[0.68,0.95], ax = ax, plot_datapoints = False , 
-                            fill_contours = True, color = 'r', smooth = 1. , linewidth=4)
+                            fill_contours = True, color = 'r', smooth = 1. , linewidth=4, range = [plot_range[3,:],plot_range[4,:]])
     ax_list = corner.hist2d(abc_sample[:,3], abc_sample[:,4], bins =20,levels=[0.68,0.95], ax = ax, plot_datapoints = False , 
-                            fill_contours = True, color = 'b', smooth = 1. , linewidth=4)
+                            fill_contours = True, color = 'b', smooth = 1. , linewidth=4, range = [plot_range[3,:],plot_range[4,:]])
     ax.plot(truths[3] , truths[4] , marker="*", markersize=25 , color = "yellow")
     ax.set_ylabel(r'$\log M_{1}$', fontsize = 50)
     ax.set_xlabel(r'$\alpha$', fontsize = 50)
@@ -99,7 +99,7 @@ def overlay_pdfs_contours(abc_filename , mcmc_filename , nwalkers , nburns , Mr)
 
     plt.legend(handles=[thick_line2, thick_line1], frameon=False, loc='best', fontsize=50)
 
-    plt.savefig("contours_nbargmf2.pdf") 
+    plt.savefig("contours_nbarxi2.pdf") 
    
     ################## HISTOGRAMS#########################################
  
@@ -107,24 +107,24 @@ def overlay_pdfs_contours(abc_filename , mcmc_filename , nwalkers , nburns , Mr)
     fig.subplots_adjust(wspace=0.4, hspace=0.2)    
    
     ax = axes[0]
-    q = ax.hist(mcmc_sample[:,2], bins =20, normed = True , alpha = 1. , color = 'r', linewidth=4 , histtype='step')
-    qq = ax.hist(abc_sample[:,2], bins =20, normed = True , alpha = 1. , color = 'b', linewidth=4 , histtype='step')
+    q = ax.hist(mcmc_sample[:,2], bins =20, range = [plot_range[2,:].min(),plot_range[2,:].max()] , normed = True , alpha = 1. , color = 'r', linewidth=4 , histtype='step')
+    qq = ax.hist(abc_sample[:,2], bins =20, range = [plot_range[2,:].min(),plot_range[2,:].max()] ,normed = True , alpha = 1. , color = 'b', linewidth=4 , histtype='step')
     ax.vlines(truths[2], 0, max(q[0].max(),qq[0].max()), color = "k", linewidth = 5)
     ax.set_xlabel(r'$\log M_{\rm min}$', fontsize = 50)
     ax.set_xlim([plot_range[2,0] , plot_range[2,1]])
     
 
     ax = axes[1]
-    q = ax.hist(mcmc_sample[:,3], bins =20, normed = True , alpha = 1. , color = 'r', linewidth=4, histtype='step')
-    qq = ax.hist(abc_sample[:,3], bins =20, normed = True , alpha = 1. , color = 'b', linewidth=4, histtype='step')
+    q = ax.hist(mcmc_sample[:,3], bins =20, range = [plot_range[3,:].min(),plot_range[3,:].max()] ,normed = True , alpha = 1. , color = 'r', linewidth=4, histtype='step')
+    qq = ax.hist(abc_sample[:,3], bins =20, range = [plot_range[3,:].min(),plot_range[3,:].max()] ,normed = True , alpha = 1. , color = 'b', linewidth=4, histtype='step')
     ax.vlines(truths[3], 0, max(q[0].max(),qq[0].max()), color = "k" , linewidth = 5)
     ax.set_xlabel(r'$\alpha$', fontsize = 50)
     ax.set_xlim([plot_range[3,0] , plot_range[3,1]])
 
 
     ax = axes[2]
-    q = ax.hist(mcmc_sample[:,4], bins =20, normed = True , alpha = 1. , color = 'r', linewidth=4, histtype='step')
-    qq = ax.hist(abc_sample[:,4], bins =20, normed = True , alpha = 1. , color = 'b', linewidth=4 , histtype='step')
+    q = ax.hist(mcmc_sample[:,4], bins =20, range = [plot_range[4,:].min(),plot_range[4,:].max()] ,normed = True , alpha = 1. , color = 'r', linewidth=4, histtype='step')
+    qq = ax.hist(abc_sample[:,4], bins =20, range = [plot_range[4,:].min(),plot_range[4,:].max()] ,normed = True , alpha = 1. , color = 'b', linewidth=4 , histtype='step')
     ax.vlines(truths[4] , 0, max(q[0].max(),qq[0].max()), colors='k' , linewidth = 5)
     ax.set_xlabel(r'$\log M_{1}$', fontsize = 50)
     ax.set_xlim([plot_range[4,0] , plot_range[4,1]])
@@ -132,12 +132,12 @@ def overlay_pdfs_contours(abc_filename , mcmc_filename , nwalkers , nburns , Mr)
 
     plt.legend(handles=[thick_line2, thick_line1], frameon=False, loc='best', fontsize=30)
 
-    plt.savefig("histograms_nbargmf2.pdf") 
+    plt.savefig("histograms_nbarxi2.pdf") 
     return None
 
 if __name__ == "__main__":
 
-    mcmc_filename = "results/nbar_gmf.mcmc.mcmc_chain.dat"
-    #abc_filename  = "results/nbar_gmf_theta_t9.abc.dat"
-    abc_filename = "results/nbar_gmf_theta_t8.ABCnbargmf.dat"
-    overlay_pdfs_contours(abc_filename , mcmc_filename , 100 , 9000 , 21) 
+    mcmc_filename = "results/nbar_xi.mcmc.mcmc_chain.dat"
+    abc_filename  = "results/nbar_xi_theta_t8.abc.dat"
+    #abc_filename = "results/nbar_gmf_theta_t8.ABCnbargmf.dat"
+    overlay_pdfs_contours(abc_filename , mcmc_filename , 100 , 6000 , 21) 
